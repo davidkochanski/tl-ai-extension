8a0ebdc1dc2bc854ce20b897476849bb977c8542
davidkochanski <kochanskidavid9@gmail.com>
2023-06-28 21:43:07 -0400

    Very messy prototype + font

--- popup.js
+++ popup.js
@@ -1,18 +1,17 @@
-// Initialize count variable
-let count = 0;
+
+let text = "";
 
 let pageContent = "Waiting...";
 
-// Get the button element
-const main = document.getElementById('main');
+const gptbtn = document.getElementById('gpt');
+const outText = document.getElementById("out");
 
-// Retrieve the count from storage if it exists
+// Retrieve the count from storage if it exists (BAD)
 browser.storage.local.get('count')
   .then(result => {
-    count = result.count ?? count;
+    text = result.count ?? text;
     pageContent = result.pageContent ?? pageContent;
 
-    updateButton();
   })
   .catch(error => {
     console.error('Error retrieving count from storage:', error);
@@ -22,14 +21,13 @@ browser.storage.local.get('count')
   .then(tabs => {
     const activeTab = tabs[0];
 
-    // Read the content of the active tab
+
     browser.tabs.sendMessage(activeTab.id, { action: "getContent" })
       .then(response => {
         const content = response.content;
         console.log("Page content:", content);
 
         pageContent = content;
-        updateButton();
       })
       .catch(error => {
         console.error("Error reading page content:", error);
@@ -39,19 +37,42 @@ browser.storage.local.get('count')
     console.error("Error getting active tab:", error);
   });
 
-// Update count and button text when the button is clicked
-main.addEventListener('click', () => {
-  count++;
-  updateButton();
+gptbtn.addEventListener('click', () => {
+interactWithChatGPT();
 
-  // Save the updated count to storage
-  browser.storage.local.set({ count: count, pageContent: pageContent })
+  browser.storage.local.set({ text: text, pageContent: pageContent })
     .catch(error => {
       console.error('Error saving count to storage:', error);
     });
 });
 
-// Function to update the button text
-function updateButton() {
-  main.textContent = `Count: ${count} I ate a ${pageContent}`;
-}
+
+function interactWithChatGPT() {
+    const token = "nice try";
+  
+
+    fetch('https://api.openai.com/v1/engines/davinci/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify({
+        prompt: `Summarize this in point form: ${pageContent}`,
+        max_tokens: 1000
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+
+        // console.log(data);
+        console.log('ChatGPT response:', data.choices[0].text);
+
+        outText.innerHTML = data.choices[0].text;
+
+      })
+      .catch(error => {
+        console.error('Error interacting with ChatGPT:', error);
+      });
+  }
+
